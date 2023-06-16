5fbd508d6e020d3da7b040c0f10a0f098fc34ce0
JH H <startingfrom33@gmail.com>
2023-06-16 12:41:47 +0900

    if-elseif-else의 모든 것

--- src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
+++ src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
@@ -11,22 +11,31 @@ public class break_continue_example1 {
 	public static void main(String[] args) throws IOException {
 
 		/* 
-		 * 문제: 10~1의 숫자를 10부터 하나씩 줄어들게 6번 반복
+		 * 문제: if와 if~else if ~ else if ~ else일 때
 		 * 문제푼날: 230616
-		 * 풀이: 나
-		 * 핵심: 맨 처음에는 10번이 출력되어야 하고 두 번째 반복될 때는 9번,
-		 * 		세 번째 반복할 때는 8번 식으로 처음 10에서 1개씩 줄어들어야 한다.
-		 *     1개씩 줄어들게 할 수 있는 방법은 j를 활용하면 된다. j가 1씩 늘어나고 있으니까!
+		 * 풀이: 선생님
+		 * 핵심: 3가지는 정확하게 무슨 차이일까?
 		 * */
 		
 		System.out.println("----------------------");
-		for(int j=0;j<6;j++) { // 6번 반복될 것이니까 j는 6번
+		for(int j=0;j<10;j++) { // 10번 반복
 			
-			for(int i=0;i<(10-j);i++) // 첫번째 i는 10번이 다 출력되어야 한다. 그러니까 10을 가지고 먼저 생각! 
-				System.out.printf("%d ", i+1);
-			
-			System.out.println();
+			for(int i=0;i<10;i++) { // 10번 반복 
+				
+//				if(i==5 || i==9)					// 조건에 해당될 때 "b"를 추가로 실행해준다. (i가 5번때랑 9번때 2번 추가 실행) 1 2 3 4 b 6 7 8 b 10
+//					System.out.print("b ");
+				
+				if(i==2)
+					System.out.print("a "); // 조건에 해당될 때 "a"를 추가로 실행하니까 더해짐 (else없으면 총 110번 실행이 되는 것)\
+				else if(i==5)
+					System.out.print("b ");
+				else if(i==9)
+					System.out.print("c ");
+				else						// 그런데 else가 있으면 나머지 경우로 조건을 제한시켜주니까 (else가 있어서 총 100번 실행)
+					System.out.printf("%d ", i+1);
 		
+			}
+			System.out.println();
 		}
 		System.out.println("----------------------");
 	}
