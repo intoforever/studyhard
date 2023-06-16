54c850a717dd2922ddfaf2aaa196bf3a662131dd
JH H <startingfrom33@gmail.com>
2023-06-16 21:18:57 +0900

    별찍기문제 (y축반전)

--- src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
+++ src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
@@ -3,39 +3,44 @@ package com.newlecture.app.ex6.control.loop;
 import java.awt.print.Printable;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Scanner;
+
+import javax.xml.stream.events.StartDocument;
 
 public class break_continue_example1 {
 
 	public static void main(String[] args) throws IOException {
 
 		/* 
-		 * 문제: while문을 이용하여, 짝수마다 +가 출력되고, 홀수마다 -가 출력되는 number작성
-		 * 			number의 값이 100이 되면 while문을 빠져나와 주세요. -+-+-+-+-+...(반복)
+		 * 문제: 다음과 같이 출력하시오
+		 * 		     *
+		 * 		    **
+		 * 		   ***
+		 * 		  ****
+		 * 		 *****
 		 * 문제푼날: 230616
 		 * 풀이: 나
-		 * 핵심: 
+		 * 핵심: 줄어드는 값과 고정된 값이 무엇인지 구분한다.
 		 * */
 		
-		
 		{
-			int number = 0;
-			
-			while(number <100) {
-				
-				if(number%2 == 0)
-					System.out.print("+");
-				else if(number%2 == 1)
-					System.out.print("-");
-			
-			number++;
-			}
-			
-		}
-		
-		
-	}
-}
-
+		String star_ = "*";
+		String star = "";
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<(5-i);j++) {
+				if(j<(5-(i+1))) {
+					System.out.print(" ");
+				} else {
+					star +=star_;
+					System.out.printf(star);
+				}
+			}//for j
+		 
+		System.out.println();	
+		}//for i
+		}//지역화
+	}//main
+}//class
 
