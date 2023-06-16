223d1162fc13a9a236157afd66a1b53c3810bda7
JH H <startingfrom33@gmail.com>
2023-06-16 10:39:04 +0900

    선생님풀이

--- src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
+++ src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
@@ -10,10 +10,19 @@ public class break_continue_example1 {
 
 	public static void main(String[] args) throws IOException {
 
+		/* 
+		 * 문제: exam.list 파일에서 2번째 만나는 100부터 100을 빼고 출력해라
+		 * 문제푼날: 230616
+		 * 풀이: 선생님
+		 * */
+		
+		
+		
 		FileInputStream fis = new FileInputStream("res/exam.list");
 		Scanner fscan = new Scanner(fis);
-
+		
 		for (int count = 0; fscan.hasNext();) {
+			
 			String word = fscan.next();
 			int kor = Integer.parseInt(word);
 
@@ -23,7 +32,7 @@ public class break_continue_example1 {
 			if (count < 2 || kor == 100)
 				continue;
 
-			System.out.printf("d,", kor);
+			System.out.printf("%d,", kor);
 
 		}
 
