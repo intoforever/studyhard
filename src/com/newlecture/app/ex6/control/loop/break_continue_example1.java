1636bb098b8e816e985c3a2dd3930fed939cea7d
JH H <startingfrom33@gmail.com>
2023-06-16 15:41:42 +0900

    짝수(+) 홀수(-) 100되면 나오기

--- src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
+++ src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
@@ -11,53 +11,31 @@ public class break_continue_example1 {
 	public static void main(String[] args) throws IOException {
 
 		/* 
-		 * 문제: (최종판)바둑판 모양을 찍자! (┼) 단, 맨 윗줄에는 (┬)을 맨 아랫줄에는 (┴)을, 양 옆에는 벽(├, ┤)을 넣자.
+		 * 문제: while문을 이용하여, 짝수마다 +가 출력되고, 홀수마다 -가 출력되는 number작성
+		 * 			number의 값이 100이 되면 while문을 빠져나와 주세요. -+-+-+-+-+...(반복)
 		 * 문제푼날: 230616
 		 * 풀이: 나
-		 * 핵심: 세로축 j와 가로축 i에서 변화되는 값이 무엇인지에 집중해보자.
+		 * 핵심: 
 		 * */
 		
 		
-		
-		for(int j=0;j<10;j++) { // 10번 반복
+		{
+			int number = 0;
 			
-			
-			for(int i=0;i<10;i++) {
-			
-				if (j == 0) {
-					if (i == 0)
-					System.out.print("┌") ;
-					else if (i == 9)
-					System.out.print("┐");
-					else
-					System.out.print("┬");
-
-				} else if (j == 9) {
-					if (i==0)
-					 System.out.print("└");
-					else if (i == 9)
-						System.out.print("┘");
-					else
-					System.out.print("┴");
-					
-					
-				} else if (i==0) {
-					System.out.print("├");
-					
-				} else if (i==9){
-					System.out.print("┤");
+			while(number <100) {
 				
-				} else {
-					System.out.print("┼"); // 아무것도 해당되지 않으면 찍기
-		
-				}
-		
+				if(number%2 == 0)
+					System.out.print("+");
+				else if(number%2 == 1)
+					System.out.print("-");
+			
+			number++;
+			}
+			
 		}
-			System.out.println();
+		
 		
 	}
-
-}
 }
 
 
