6f5a325084bf14ffa2440ad700b115507274cff2
JH H <startingfrom33@gmail.com>
2023-06-16 11:11:43 +0900

    내가푼풀이4

--- src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
+++ src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
@@ -11,16 +11,17 @@ public class break_continue_example1 {
 	public static void main(String[] args) throws IOException {
 
 		/* 
-		 * 문제: 1 2 3 4 5 6 7 8 9 10의 문장을 10번 반복해서 출력해라
+		 * 문제: 1 2 3 4 5 6 7 8 9 10의 문장을 1부터 하나씩 늘어나서 10번 반복해서 삼각형 모양으로 출력해라
 		 * 문제푼날: 230616
-		 * 풀이: 선생님 풀이
-		 * 핵심: for을 2번 반복시키는 것이다.
+		 * 풀이: 나
+		 * 핵심: 안에 있는 밖에 있는 for2문이 반복될수록 안에 있는 for1문의 반복 횟수가 같이 늘어나야 한다
+		 *     (반복횟수가 고정되어 있으면 안되는구나!) 
 		 * */
 		
 		System.out.println("----------------------");
 		for(int j=0;j<10;j++) {
 			
-			for(int i=0;i<10;i++)
+			for(int i=0;(j+1)-i>0;i++) // 기혁님 풀이: i<=j로 하셨다. j만큼 반복되어야 하니까
 				System.out.printf("%d ", i+1);
 			
 			System.out.println();
