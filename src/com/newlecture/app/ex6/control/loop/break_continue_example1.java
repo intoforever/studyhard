ad80c9bfedfb62e604fd3c19edf6f0ae2d23f185
JH H <startingfrom33@gmail.com>
2023-06-16 11:01:56 +0900

    선생님풀이3

--- src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
+++ src/com/newlecture/app/ex6/control/loop/break_continue_example1.java
@@ -13,10 +13,11 @@ public class break_continue_example1 {
 		/* 
 		 * 문제: 1 2 3 4 5 6 7 8 9 10의 문장을 10번 반복해서 출력해라
 		 * 문제푼날: 230616
-		 * 풀이: 나
-		 * 핵심: 
+		 * 풀이: 선생님 풀이
+		 * 핵심: for을 2번 반복시키는 것이다.
 		 * */
 		
+		System.out.println("----------------------");
 		for(int j=0;j<10;j++) {
 			
 			for(int i=0;i<10;i++)
@@ -25,14 +26,9 @@ public class break_continue_example1 {
 			System.out.println();
 		
 		}
-		
-				
-		
-		
-			
-
-		}
-
+		System.out.println("----------------------");
 	}
+
+}
 
 
