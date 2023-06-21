a08d72f8ee7f4f925042252c9b6c42c6ed97f8fc
JH H <startingfrom33@gmail.com>
2023-06-21 10:13:25 +0900

    배열만들기1

---
+++ src/ex06/array/basic/Program.java
@@ -0,0 +1,18 @@
+package ex06.array.basic;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		/* 
+		 * 문제: 영어 성적 3개를 저장하기 위한 정수형 배열 engs를 생성하시오.
+		 * 풀이날짜: 230621
+		 * 
+		 */
+		
+		int[] engs = new int[3];
+		
+		
+	}
+
+}
